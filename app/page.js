69348c74d59d89dece50ca6c6eda99c7bfe06a28
rajib <rajibrad@gmail.com>
2025-05-19 15:56:34 +0600

    create course model and testing find query

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Test from "@/components/ui/Test";
+import { getCourses } from "@/queries/courses";
 
 
-export default function Home() {
+export default async function Home() {
+  const allCourses = await getCourses()
+  console.log(allCourses);
   return (
     <Test />
+
   );
 }
--- model/course-model.js
+++ model/course-model.js
@@ -1,4 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 
 
-const courseSchema = new Schema()
+const courseSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    modules: { type: [Schema.ObjectId], required: false }, //property ta ekta array of string
+    price: { type: Number, required: true },
+    active: { type: Boolean, required: true },
+    category: { type: Schema.ObjectId, required: false },
+    instructor: { type: Schema.ObjectId, required: false },
+    testimonials: { type: [Schema.ObjectId], required: false },
+    quizSet: { type: Schema.ObjectId, required: false }
+
+})
+
+
+export const Course = mongoose.models.Course ?? mongoose.model("Course", courseSchema)
