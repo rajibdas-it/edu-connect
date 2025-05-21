13888509d9b56d7c8387f330cc9ef14f5880e33a
rajib <rajibrad@gmail.com>
2025-05-21 12:27:31 +0600

    customize service file for connect db automatically

--- service/mongo.js
+++ service/mongo.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose"
 
 export async function dbConnect() {
     try {
-        const conn = await mongoose.connect(String(process.env.MONGODB_CONN_STRING))
+        const conn = await mongoose.connect(String(process.env.MONGODB_CONN_STRING), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 10000 // 10 seconds
+        })
         return conn
     } catch (err) {
         console.error(err)
