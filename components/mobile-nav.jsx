e9706d266e8d14f3221f39eb398c2348fef2e028
rajibdas-it <rajibrad@gmail.com>
2025-05-24 01:24:16 +0600

    get course and category from db

--- components/mobile-nav.jsx
+++ components/mobile-nav.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
-// import { useLockBody } from "@/hooks/use-lock-body";
+import { useLockBody } from "@/hooks/use-lock-body";
 import {
   DropdownMenu,
   DropdownMenuContent,
--- queries/courses.js
+++ queries/courses.js
@@ -1,3 +1,4 @@
+import { replaceMongoIdInArray } from "@/lib/convertData";
 import { Category } from "@/model/category-model";
 import { Course } from "@/model/course-model";
 import { Module } from "@/model/module-model";
@@ -6,19 +7,21 @@ import { User } from "@/model/user-model";
 
 
 
-export async function getCourses() {
-    const courses = await Course.find({}).populate({
-        path: "category",
-        model: Category
-    }).populate({
-        path: "instructor",//course model er kon field hobe.
-        model: User
-    }).populate({
-        path: "testimonials",//course model er kon field hobe.
-        model: Testimonial
-    }).populate({
-        path: "modules",//course model er kon field hobe.
-        model: Module
-    })
-    return courses
+export async function getCourseList() {
+    const courses = await Course.find({}).select(["title", "subtitle",
+        "thumbnail", "modules", "price",
+        "category", "instructor", "testimonials"]).populate({
+            path: "category",
+            model: Category
+        }).populate({
+            path: "instructor",//course model er kon field hobe.
+            model: User
+        }).populate({
+            path: "testimonials",//course model er kon field hobe.
+            model: Testimonial
+        }).populate({
+            path: "modules",//course model er kon field hobe.
+            model: Module
+        }).lean()
+    return replaceMongoIdInArray(courses)
 }
